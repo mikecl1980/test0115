bfffc31ef4486ae8a038764ca1f344dbb293851d
mikecl1980 <28712720@qq.com>
2019-01-15 10:33:32 +0800

    Initial commit

---
+++ src/ThreadTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ThreadTest {
+    public static final int num=1;
+    public static void main(String[] args)
+    {
+        System.out.println(123);
+        List list = new ArrayList();
+        list.add(0,1);
+        Map map = new HashMap();
+        map.put("aaa",123);
+        String aaaab;
+
+
+    }
+
+
+}
